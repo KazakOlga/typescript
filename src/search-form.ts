b5c05ec535271e23efea1d4c53715664e1814063
Olga <astanacards001@gmail.com>
2021-10-24 00:25:40 +0300

    lesson1

---
+++ src/search-form.ts
@@ -0,0 +1,49 @@
+import { renderBlock } from './lib.js'
+
+export function renderSearchFormBlock () {
+  let entryDate;
+  let departureDate;
+  const date = new Date()
+  const minEntryDate = Date(date.setDate(date.getDate()+1))
+  const maxEntryDate = Date(date.setFullYear(date.getFullYear()+1))
+  const minDepartureDate = Date(date.setDate(date.getDate()+3))
+  const maxDepartureDate = Date(date.setFullYear(date.getFullYear()+1))
+
+  renderBlock(
+    'search-form-block',
+    `
+    <form>
+      <fieldset class="search-filedset">
+        <div class="row">
+          <div>
+            <label for="city">Город</label>
+            <input id="city" type="text" disabled value="Санкт-Петербург" />
+            <input type="hidden" disabled value="59.9386,30.3141" />
+          </div>
+          <!--<div class="providers">
+            <label><input type="checkbox" name="provider" value="homy" checked /> Homy</label>
+            <label><input type="checkbox" name="provider" value="flat-rent" checked /> FlatRent</label>
+          </div>--!>
+        </div>
+        <div class="row">
+          <div>
+            <label for="check-in-date">Дата заезда</label>
+            <input id="check-in-date" type="date" value="${entryDate}" min="${minEntryDate}" max="${maxEntryDate}" name="checkin" />
+          </div>
+          <div>
+            <label for="check-out-date">Дата выезда</label>
+            <input id="check-out-date" type="date" value="${departureDate}" min="${minDepartureDate}" max="${maxDepartureDate}" name="checkout" />
+          </div>
+          <div>
+            <label for="max-price">Макс. цена суток</label>
+            <input id="max-price" type="text" value="" name="price" class="max-price" />
+          </div>
+          <div>
+            <div><button>Найти</button></div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    `
+  )
+}
